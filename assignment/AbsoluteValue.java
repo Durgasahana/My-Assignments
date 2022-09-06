3bf428458fb670cfff6483cb6fe3869de6f2f6e5
Durgasahana <111432560+Durgasahana@users.noreply.github.com>
2022-09-06 20:32:51 +0530

    Add files via upload
    Day2

---
+++ assignment/AbsoluteValue.java
@@ -0,0 +1,19 @@
+package week1.day2.assignment;
+
+public class AbsoluteValue {
+
+	public static void main(String[] args) {
+		int num=-40;
+		if(num<0)
+		{
+			int num1=-(num);
+			System.out.println("The number"+num+" is converted into +"+num1);
+		}
+		else
+		{
+			System.out.println(num+"is a positive number");
+		}
+
+	}
+
+}
